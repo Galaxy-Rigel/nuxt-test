5fb703f0caa2fb071c7652d6a5d6d2060ad2eb2e
yueming.liu <822616692@qq.com>
2021-10-21 12:39:57 +0800

    修改pm2启动方式

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nuxtApp',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/.bin/nuxt',
+      args: 'start --port=4001',
+    },
+  ],
+}
